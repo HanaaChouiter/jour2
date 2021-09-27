79074ab9c6e5345906d0e4b2ba4882ecfc1ba7be
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-09-27 17:22:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var integer = 102
+var float = 13.9
+//console.log(integer)
+//console.log(float)
+
+var basic = 34
+var stringified = basic.toString();
+//console.log(stringified)
+
+var num = 1.5
+var rounded = Math.round(num)
+//console.log(rounded)
+
+var test = 12
+var bis = 5
+//console.log(test + bis)
+//console.log(test - bis)
+//console.log(test * bis)
+//console.log(test / bis)
+//console.log(test ** bis)
+//console.log(test % bis)
+
+var test = 143
+var bis = 219
+//console.log(test > bis) // strictement supérieur
+//console.log(test < bis) // strictement inférieur
+//console.log(test >+ bis) // supérieur ou égal
+//console.log(test <= bis) // inférieur ou égal
+//console.log(test == bis) // égal à
+//console.log(test === bis) // strictement égal à
+//console.log(test != bis) // différent de
+//console.log(test !== bis) // strictement différent de
+
+var limit = 50
+var score = 6
+
+if (score >= limit){
+    //console.log("Ok good !")
+}else{
+    //console.log("Oh nooo...")
+}
+
+var password = "azerty";
+
+if(password.length > 5){
+    //console.log("The password is secure")
+}
+
+if(score >= limit && password.length > 5){
+    //console.log("Everything is good")
+}else if 
+    (score >= limit || password.length > 20){
+        //console.log("Something is good")
+    }else{
+        //console.log("Nothing is good")
+    }
+
+var random = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+
+if(random === 6){
+    //console.log("Yes I win")
+}else{
+    //console.log("So close...")
+}
+
+var month = "April"
+
+switch (month) {
+	case "January": 
+		console.log("Winter");
+		break;
+	case "April": 
+		console.log("spring");
+		break;
+	case "July": 
+		console.log("Summer");
+		break;
+	case "September": 
+		console.log("fall");
+		break;
+	default: 
+		console.log("no");
+}
+
+var roundeNumber = 3.6
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
